2a98f2be99432018e9438a6986d3a9b602773de0
Fernando Nardes <frnnardes@gmail.com>
2025-06-14 20:59:54 -0300

    feat: Adicionando sistema de alocacao e remocao aleatoria de espacos, alem do GC simulado

--- Main.java
+++ Main.java
@@ -1,24 +1,19 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/simulacao.fxml")); // Carrega o FXML
-        Parent root = loader.load(); // Define o root com o FXML e carrega o controller atraves do metodo load
-        Scene scene = new Scene(root); // Define a cena com o root
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/simulacao.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("GC Visualizer - Protótipo Inicial");
+        stage.show();
+    }
 
-        primaryStage.setScene(scene); // Define a cena no Stage
-        primaryStage.setTitle("Garbage Collector"); // Define o titulo do Stage
-        // primaryStage.initStyle(StageStyle.UNDECORATED); // Estiliza o Stage, removendo as bordas
-        primaryStage.setResizable(false); // Define o Stage como nao redimensionavel
-        primaryStage.show(); // Exibe o Stage para o usuario
-    } // Fim do metodo start
-  public static void main(String[] args) {
-    launch(args);
-  } // Fim do metodo main
-}// Fim da classe Principal
+    public static void main(String[] args) {
+        launch();
+    }
+}
